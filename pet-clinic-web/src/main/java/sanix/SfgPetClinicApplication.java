ac9b01ffb8b5e9fd8bdc63d040d71af73a5bc81d
sanix sandel <sanicksikani@gmail.com>
2020-08-21 22:17:57 +0300

    Index Page and Controller

--- pet-clinic-web/src/main/java/sanix/SfgPetClinicApplication.java
+++ pet-clinic-web/src/main/java/sanix/SfgPetClinicApplication.java
@@ -1,3 +1,5 @@
+package sanix;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
---
+++ pet-clinic-web/src/main/java/sanix/controllers/indexController.java
@@ -0,0 +1,16 @@
+package sanix.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class indexController {
+
+    @RequestMapping({"", "/", "index", "index.html"})
+    public String index(){
+        return "index";
+    }
+
+
+
+}
