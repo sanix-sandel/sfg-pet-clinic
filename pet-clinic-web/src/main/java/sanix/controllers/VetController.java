88f38f90b36df8b69f9722289db6284d7c10a06f
sanix sandel <sanicksikani@gmail.com>
2020-08-29 04:19:21 +0300

    List Vets. Closes #15

--- pet-clinic-web/src/main/java/sanix/controllers/VetController.java
+++ pet-clinic-web/src/main/java/sanix/controllers/VetController.java
@@ -1,13 +1,23 @@
 package sanix.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import sanix.services.VetService;
 
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
-    public String ListVets(){
+    public String ListVets(Model model){
+
+        model.addAttribute("vets", vetService.findAll());
 
         return "vets/index";
     }
